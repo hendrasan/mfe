9f17fde1dba6ad308fd97b3b18a16fd29b2aa1b8
Hendra Susanto <anotherhendrasusanto@gmail.com>
2022-08-10 23:46:05 +0700

    initial commit

---
+++ packages/marketing/src/components/Nav.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    a: {
+      textDecoration: "none",
+    },
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+export default function Nav() {
+  const classes = useStyles();
+  return (
+    <Container maxWidth="md">
+      <Link to="/">
+        <Button variant="contained" color="primary">
+          Home
+        </Button>
+      </Link>
+    </Container>
+  );
+}
