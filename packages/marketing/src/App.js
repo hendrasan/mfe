2f2458d7b41f7ca7fbe33330ad3afda18a71a3b8
Hendra Susanto <anotherhendrasusanto@gmail.com>
2022-08-14 19:10:18 +0700

    fix: added production prefix to classnames for both applications

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 import Nav from "./components/Nav";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Nav />
           <Switch>
